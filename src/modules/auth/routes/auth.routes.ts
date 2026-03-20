184ae70d4aa0618cbc4c3b70d4bb2ba35b0a0da7
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-20 19:48:40 -0300

    feat: add authentication middleware to the me route for user verification

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,15 +1,17 @@
-import Router from "express";
+import {Router} from "express";
 import { UserRepository } from "../../user/repository/user.repository";
 import { AuthController } from "../controller/auth.controller";
 import { AuthService } from "../service/auth.service";
+import { AuthMiddleware } from "../middleware/auth.middleware";
 
 const authRoutes = Router();
 const userRepository = new UserRepository();
 const authService = new AuthService(userRepository);
 const authController = new AuthController(authService);
+const authMiddleware = new AuthMiddleware();
 
 authRoutes.post("/register", authController.register);
 authRoutes.post("/login", authController.login);
-authRoutes.get("/me", authController.me);
+authRoutes.get("/me", authMiddleware.authenticate, authController.me);
 
 export { authRoutes };
