b1f3eddd694eaa6c9c6bc2216654fb9ccfefbc93
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-27 16:01:14 -0300

    feat: add appointment routes for creating and listing appointments by client ID

---
+++ src/modules/appointments/routes/appointment.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { AppointmentController } from "../controller/appointment.controller";
+import { AppointmentService } from "../service/appointment.service";
+import { AppointmentRepository } from "../repository/appointment.repository";
+import { AuthMiddleware } from "../../auth/middleware/auth.middleware";
+import { ClientRepository } from "../../clients/repository/client.repository";
+
+
+const appointmentRepository = new AppointmentRepository();
+const clientRepository = new ClientRepository();
+const appointmentService = new AppointmentService(appointmentRepository, clientRepository);
+const appointmentController = new AppointmentController(appointmentService);
+const authMiddleware = new AuthMiddleware();
+
+const appointmentRouter = Router();
+
+appointmentRouter.post("/", authMiddleware.authenticate, (req, res) => appointmentController.create(req, res));
+appointmentRouter.get("/:clientId", authMiddleware.authenticate, (req, res) => appointmentController.listByClientId(req, res));
+
+export { appointmentRouter };
