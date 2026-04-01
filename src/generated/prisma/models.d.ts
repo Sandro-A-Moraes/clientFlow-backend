9536f3299ee39b96066b888391d52226dd96e5ea
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-04-01 11:49:37 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/generated/prisma/models.d.ts
@@ -0,0 +1,5 @@
+export type * from './models/User';
+export type * from './models/Client';
+export type * from './models/Appointment';
+export type * from './commonInputTypes';
+//# sourceMappingURL=models.d.ts.map
